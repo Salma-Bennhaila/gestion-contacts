83a0155cac4072173e275e581c36c31306d5c413
Salma Bennhaila <33033599+Salma-Bennhaila@users.noreply.github.com>
2019-04-22 11:52:45 +0100

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -6,15 +6,5 @@ use Illuminate\Support\Facades\Storage;
 
 class Helper
 {
-    public static function applyActiveClass(?ViewContext $context, $link): string
-    {
-        return !empty($context) && $context->isActive($link) ? 'active' : '';
-    }
-
-    public static function deletePublicFile($publicPath)
-    {
-        $path = str_replace('storage/', 'public/', $publicPath);
-
-        Storage::delete($path);
-    }
+   
 }
